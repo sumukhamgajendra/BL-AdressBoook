6af68c1c59e55e2d5fbac79ae135b32be1f7192d
SumukhaMG <sumukhamg77266@gmail.com>
2026-01-19 12:02:10 +0530

    [Sumukha:modefied]Modefied Addressbook class, Now user can add contacts to addressBook

--- src/models/Addressbook.py
+++ src/models/Addressbook.py
@@ -1,13 +1,11 @@
 import models.Contacts
 
-
-
 class AddressBook:
     def __init__(self):
-        self.contacts = []
+        self.address_book = {'default': []}
 
     def add_contact(self, contact):
-        self.contacts.append(contact)
+        self.address_book['default'].append(contact)
 
     def get_info(self):
         f_name = input("First Name: ")
@@ -21,11 +19,11 @@ class AddressBook:
         self.add_contact(new_contact)
 
     def display_contacts(self):
-        if not self.contacts:
+        if not self.address_book['default']:
             print("Address book is empty.")
             return
 
-        for contact in self.contacts:
+        for contact in self.address_book['default']:
             print("Contact Details:")
             print(f"first name: {contact.f_name},\n last name: {contact.l_name},\n city: {contact.city},\n state: {contact.state},\n zip code: {contact.zip_code},\nphone number: {contact.phone},\nemail: {contact.email}")
             print()
